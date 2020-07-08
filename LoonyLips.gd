23cb20faa81c25510388cc716bc04f822154b72c
Gagandeep Singh <gagan.ddun@gmail.com>
2020-07-08 23:09:23 +1000

    added button

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -4,13 +4,19 @@ func _ready():
 	var prompts = ["Yann", "banana", "tingly", "glorious"]
 	
 	var story = "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day."
-	# get_node("Label").text = "Hey"
+
 	$VBoxContainer/DisplayText.text = story % prompts
 
 
 func _on_PlayerText_text_entered(new_text):
 	update_DisplayText(new_text)
 
+
+func _on_TextureButton_pressed():
+	var words = $VBoxContainer/HBoxContainer/PlayerText.text
+	update_DisplayText(words)
+
+
 func update_DisplayText(words):
 	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/PlayerText.clear()
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
