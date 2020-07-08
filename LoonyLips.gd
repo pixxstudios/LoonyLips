ca51135b1b66a5d8a1c3d759e01e28e69c243a57
Gagandeep Singh <gagan.ddun@gmail.com>
2020-07-08 22:57:36 +1000

    take user input

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -5,4 +5,12 @@ func _ready():
 	
 	var story = "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was a %s day."
 	# get_node("Label").text = "Hey"
-	$DisplayText.text = story % prompts
+	$VBoxContainer/DisplayText.text = story % prompts
+
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/PlayerText.clear()
